b491c7adff3bffe56e814bc090175fef0162c865
Gregor Doroschenko <g.doroschenko@gmail.com>
2018-03-05 00:45:35 +0100

    extend routing by about and contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,17 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
